3132a1de035ae69443e3038c53a5ba193525aa70
Minkyu Lee <manzi@kakao.com>
2024-09-02 05:31:43 +0000

    update App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AudioReactiveHypnotic from './components/AudioReactiveHypnotic';
+
+function App() {
+  return (
+    <div className="App">
+      <AudioReactiveHypnotic />
+    </div>
+  );
+}
+
+export default App;
